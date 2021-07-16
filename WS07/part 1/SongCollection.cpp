c6d93e1bac20dfd4b0fa922088006ba04f5a3142
jianylei <jianyonglei@gmail.com>
2021-07-16 13:32:59 -0400

    songcollection modules - implemented "Song" struct

---
+++ WS07/part 1/SongCollection.cpp
@@ -0,0 +1,10 @@
+/*
+Danny Lei - 164700197
+07/16/21
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+#include "SongCollection.h"
+namespace sdds {
+
+   
+}
---
+++ WS07/part 1/SongCollection.h
@@ -0,0 +1,20 @@
+/*
+Danny Lei - 164700197
+07/16/21
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+#ifndef SDDS_SONGCOLLECTION_H_
+#define SDDS_SONGCOLLECTION_H_
+#include <string>
+namespace sdds {
+    struct Song {
+        std::string m_artist;
+        std::string m_song;
+        std::string m_album;
+        double price{ 0 };
+        size_t m_releaseYear{ 0u };
+        size_t m_length{ 0u };
+    };
+   
+}
+#endif
