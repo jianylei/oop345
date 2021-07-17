cbcf4a25c814eb7967c57b7b24d75b6175ec42fc
jianylei <jianyonglei@gmail.com>
2021-07-17 17:39:18 -0400

    finished prototypes to workstation class functions

---
+++ prj/MS3/Workstation.cpp
@@ -0,0 +1,12 @@
+// Name: Danny Lei
+// Seneca Student ID: 164700197
+// Seneca email: jylei1@senecacollege.ca
+// Date of completion: 07/17/21
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+#include "Workstation.h"
+namespace sdds {  
+
+   
+}
