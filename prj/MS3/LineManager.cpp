2788fb5898a5af21b37f7a04140d819db8dd8093
jianylei <jianyonglei@gmail.com>
2021-07-19 23:05:01 -0400

    ws08 init

--- prj/MS3/LineManager.cpp
+++ prj/MS3/LineManager.cpp
@@ -5,8 +5,38 @@
 //
 // I confirm that I am the only author of this file
 //   and the content was created entirely by me.
+#include <fstream>
+#include <algorithm>
+#include "Utilities.h"
 #include "LineManager.h"
 namespace sdds {  
-
+    LineManager::LineManager(const std::string& file, const std::vector<Workstation*>& stations) {
+        std::fstream openedFile(file);
+        if(!openedFile) { throw "File could not be opended"; }
+        else {
+            std::string line;
+            std::string str;
+            size_t pos{ 0u };
+            bool more = true;
+            Workstation * currentStation{ nullptr };
+            Workstation * nextStation{ nullptr };
+            Utilities utils;
+            
+            while(getline(openedFile, line)) {
+                str = utils.extractToken(line, pos, more);
+                currentStation = *std::find_if(stations.begin(), stations.end(), [](Workstation* stations){
+                    return stations->getItemName() == str;
+                });
+                if(more) {
+                    str = utils.extractToken(line, pos, more);
+                    nextStation = *std::find_if(stations.begin(), stations.end(), [](Workstation* stations){
+                        return stations->getItemName() == str;
+                    }); 
+                    currentStation->setNextStation(nextStation);
+                }
+                activeLine.push_back(currentStation);
+            }
+        }
+    }
    
 }
--- prj/MS3/LineManager.h
+++ prj/MS3/LineManager.h
@@ -7,8 +7,18 @@
 //   and the content was created entirely by me.
 #ifndef SDDS_LINEMANAGER_H
 #define SDDS_LINEMANAGER_H
+#include <vector>
+#include "Workstation.h"
 namespace sdds {  
-    
+    class LineManager {
+        std::vector<Workstation*> activeLine;
+        size_t m_cntCustomerOrder{ 0u };
+        Workstation* m_firstStation{ nullptr };
+    public:
+        LineManager(const std::string& file, const std::vector<Workstation*>& stations);
+        void linkStations();
+        bool run(std::ostream& os);
+    };
    
 }
 #endif
