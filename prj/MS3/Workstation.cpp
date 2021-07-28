0bfb110b067e34b2957d3482b55de2af999815c3
jianylei <jianyonglei@gmail.com>
2021-07-28 14:30:40 -0400

    test

--- prj/MS3/Workstation.cpp
+++ prj/MS3/Workstation.cpp
@@ -4,7 +4,7 @@
 // Date of completion: 07/17/21
 //
 // I confirm that I am the only author of this file
-//   and the content was created entirely by me.
+//   and the content was created entirely by me
 #include <utility>
 #include <iostream>
 #include "Workstation.h"
